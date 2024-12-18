3287d8c496b27c6b6701c8e941232a9476609c26
cc-santiago-alvarez <santiago.salinas@finoservices.com>
2024-12-18 10:29:55 -0500

    feat cambios pa que funcione

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import os
-import io
 import tempfile
-import base64
-from werkzeug.datastructures import FileStorage
-from flask import Flask, request, jsonify, send_file, render_template, url_for
+from flask import Flask, request, jsonify, send_file, render_template
 from flask_cors import CORS
 from convert import convert_file_to_pdf
 
@@ -11,6 +8,9 @@ app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})  # Asegúrate de permitir el origen necesario
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+# Carpeta temporal para almacenar archivos
+TEMP_FOLDER = "./temp/"
+os.makedirs(TEMP_FOLDER, exist_ok=True)
 
 @app.route('/')
 def home():
@@ -19,74 +19,41 @@ def home():
 @app.route('/api/v1/convert/convert-to-pdf', methods=['POST'])
 def convert():
     try:
-        if 'file[0]' not in request.files:
+        if 'file' not in request.files:
             return jsonify({'error': 'No se encontró el campo "file".'}), 400
-         
-        results = []
-        count =0
-        
-        for _, uploaded_file in request.files.items():
-             
-            count += 1 
-            print(f"Procesando archivo {count}: {uploaded_file.filename}")
-           
-            original_filename = uploaded_file.filename
-            
-            originalName = os.path.splitext(original_filename)[0]
-            
-            extension = '.pdf'
-            
-            input_path = io.BytesIO(uploaded_file.read()) 
-            pdf_buffer = convert_file_to_pdf(input_path, original_filename)
-            
-            pdf_buffer.seek(0)  # Reiniciar el puntero del archivo
-            pdf_bytes = list(pdf_buffer.read())
-            
-            results.append({
-                'file': pdf_bytes,
-                'originalName': f"{originalName}{extension}"
-            })
-            
-            
-             # Retornar la respuesta con los archivos procesados
-        return jsonify({
-            'success': True,
-            'count': len(results),  # Cantidad de archivos procesados
-            'files': results
-        })
-        
-        
+
+        uploaded_file = request.files['file']
+        input_path = os.path.join(TEMP_FOLDER, uploaded_file.filename)
+        uploaded_file.save(input_path)
+
+        output_path = convert_file_to_pdf(input_path)
+
+        if not isinstance(output_path, str):
+            raise ValueError(f"La función convert_file_to_pdf devolvió un valor inesperado: {output_path}")
+
+        file_url = f"http://{request.host}/download/{os.path.basename(output_path)}"
+        return jsonify({'success': True, 'pdf_url': file_url})
+
     except Exception as e:
-        print("Error:", str(e))
         return jsonify({'error': str(e)}), 500
 
-            
-        
-        # if not isinstance(output_path, str):
-        #     raise ValueError(f"La función convert_file_to_pdf devolvió un valor inesperado: {output_path}")
-        
-        # # Leer el archivo convertido como bytes
-        # with open(output_path, 'rb') as pdf_file:
-        #     pdf_bytes = pdf_file.read()
-            
-        #     results.append(
-        #      {
-        #         'file': list(pdf_bytes),
-        #         'originalName': f"{originalName}{extension}",
-        #     }
-        #     )
-            
-            # print("sisas----------------->",results)
 
-    #     return jsonify({ 
-    #         'success': True,
-    #         'files': results
-    #     })
-     
-    # except Exception as e:
-    #     return jsonify({'error': str(e)}), 500
+@app.route('/download/<filename>', methods=['GET'])
+def download_file(filename):
+    try:
 
-       
+        file_path = os.path.join(TEMP_FOLDER, filename)
+
+        if not os.path.exists(file_path):
+            return jsonify({'error': 'Archivo no encontrado'}), 404
+
+        print(f"Enviando archivo: {file_path}", flush=True)
+
+        return send_file(file_path, as_attachment=True)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='10.2.20.113', port=25268)
-
--- convert.py
+++ convert.py
@@ -1,7 +1,5 @@
 import os
 import sys
-import io
-from typing import BinaryIO
 from formats.docx.docx import convert_docx_to_pdf
 from formats.xlsx.xlsx import convert_xlsx_to_pdf
 from formats.pptx.pptx import convert_pptx_to_pdf
@@ -9,73 +7,46 @@ from formats.images.images import convert_image_to_pdf
 from formats.txt.txt import convert_txt_to_pdf
 from flask import jsonify
 
-def convert_file_to_pdf(input_path: BinaryIO, filename: str) -> io.BytesIO:
-    try:
-        # ext = os.path.splitext(input_path)[1].lower()
-        # base_name = os.path.splitext(os.path.basename(input_path))[0]
+def download_pdf():
+    return os.path.join(os.path.expanduser('~'), 'Downloads')
 
-        # downloads = download_pdf()
-        # output_path = generate_unique_filename(downloads, base_name, '.pdf')
-        ext = os.path.splitext(filename)[1].lower()
-        pdf_buffer = io.BytesIO()  # Crear un buffer en memoria para el PDF
+def generate_unique_filename(base_path, base_name, extension):
+    counter = 1
+    new_path = os.path.join(base_path, f"{base_name}{extension}")
+    while os.path.exists(new_path):
+        new_path = os.path.join(base_path, f"{base_name}_{counter}{extension}")
+        counter += 1
+    return new_path
+
+def convert_file_to_pdf(input_path: str, output_path: str = "") -> str:
+    try:
+        ext = os.path.splitext(input_path)[1].lower()
+        base_name = os.path.splitext(os.path.basename(input_path))[0]
+
+        downloads = download_pdf()
+        output_path = generate_unique_filename(downloads, base_name, '.pdf')
 
         if ext == '.docx':
-            convert_docx_to_pdf(input_path, pdf_buffer)
+            convert_docx_to_pdf(input_path, output_path)
         elif ext == '.xlsx':
-            convert_xlsx_to_pdf(input_path, pdf_buffer)
+            convert_xlsx_to_pdf(input_path, output_path)
         elif ext == '.pptx':
-            convert_pptx_to_pdf(input_path, pdf_buffer)
+            convert_pptx_to_pdf(input_path, output_path)
         elif ext == '.txt':
-            convert_txt_to_pdf(input_path, pdf_buffer)
+            convert_txt_to_pdf(input_path, output_path)
         elif ext in ['.svg', '.png', '.jpg', '.jpeg']:
-            convert_image_to_pdf(input_path, pdf_buffer)
+            convert_image_to_pdf(input_path, output_path)
         else:
             raise ValueError(f"INVALID_FILE_FORMAT: {ext}")
 
-        print(f"Archivo PDF generado en: {pdf_buffer}")
-        return pdf_buffer
+        print(f"Archivo PDF generado en: {output_path}")
+        return output_path
 
 
     except Exception as e:
         error_message = f"CONVERSION_ERROR: {str(e)}"
         print(error_message)
         raise ValueError(error_message)
-
-# def convert_file_buffer_to_pdf(file_buffer: BinaryIO, filename: str) -> bytes:
-#     """
-#     Convierte un archivo en buffer (stream) a PDF y devuelve el PDF como bytes.
-
-#     :param file_buffer: Archivo en buffer (BinaryIO o BytesIO).
-#     :param filename: Nombre del archivo original para identificar su tipo.
-#     :return: El PDF generado como bytes.
-#     """
-#     file_extension = filename.lower().split('.')[-1]
-#     output_buffer = io.BytesIO()  # Donde almacenaremos el PDF generado
-
-#     try:
-#         if file_extension == "docx":
-#             # Convertir DOCX a PDF
-#             convert_docx_to_pdf(file_buffer, output_buffer)
-#         elif file_extension == "xlsx":
-#             # Convertir XLSX a PDF
-#             convert_xlsx_to_pdf(file_buffer, output_buffer)
-#         elif file_extension == "pptx":
-#             # Convertir PPTX a PDF
-#             convert_pptx_to_pdf(file_buffer, output_buffer)
-#         elif file_extension in ["png", "jpg", "jpeg", "svg"]:
-#             # Convertir imágenes a PDF
-#             convert_image_to_pdf(file_buffer, output_buffer)
-#         elif file_extension == "txt":
-#             # Convertir TXT a PDF
-#             convert_txt_to_pdf(file_buffer, output_buffer)
-#         else:
-#             raise ValueError(f"Formato de archivo no soportado: {file_extension}")
-#     except Exception as e:
-#         raise ValueError(f"Error al convertir archivo: {e}")
-
-#     # Retornar el PDF generado como bytes
-#     output_buffer.seek(0)
-#     return output_buffer.read()
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -84,6 +55,7 @@ if __name__ == '__main__':
 
     input_file = sys.argv[1]
 
+    # Llama a la función de conversión y obtiene la ruta del PDF
     try:
         pdf_path = convert_file_to_pdf(input_file)
         print(f"PDF generado en: {pdf_path}")
